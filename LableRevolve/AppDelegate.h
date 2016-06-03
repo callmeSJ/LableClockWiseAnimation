c2f611fedaff3a02840dd0fc132f33ed429be47a
SJ <shijie7575@foxmail.com>
2016-06-03 15:34:45 +0800

    Initial Commit

---
+++ LableRevolve/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LableRevolve
+//
+//  Created by SJ on 16/6/3.
+//  Copyright © 2016年 SJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
